486caff1e3b6193f43a6df8894bbe7352a70dfbc
Hannah Wlasowicz <hannahwl@buffalo.edu>
2020-05-22 19:32:52 -0400

    quasar files

---
+++ trash-to-treasure/src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class = "center">
+      <h1>Your Cordinates</h1>
+      <p>{{  coordinates.lat  }} Latitude, {{  coordinates.lng  }} Longitude</p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      coordinates: {
+        lat: 0,
+        lng: 0
+      }
+    }
+  },
+  create () {
+  // get info from user
+
+  },
+  name: 'PageIndex'
+}
+</script>
